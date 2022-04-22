4841155280bc030a3f743ceef37b7601be089714
Dmitriy Yudichev <dimarik34rus@ya.ru>
2022-04-22 16:15:36 +0300

    Upload Accordion_JS

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// Accordon
+const accordions = document.querySelectorAll('.accordion__item');
+
+for(item of accordions) {
+	item.addEventListener('click', function() {
+		if(this.classList.contains('active')) {
+			this.classList.remove('active');
+		} else {
+			for(el of accordions) {
+				el.classList.remove('active');
+			}
+			this.classList.add('active');
+		}
+	})
+}
